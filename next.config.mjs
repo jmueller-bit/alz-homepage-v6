e4bd09ca9380bdbcef6c00386afb39ac00892834
Juergen Mueller <j.mueller@astrid-lindgren-zentrum.at>
2026-02-24 23:18:00 +0100

    Add Notion S3 domain to allowed image hosts
    - Allow prod-files-secure.s3.us-west-2.amazonaws.com for Notion images

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+      },
     ],
   },
   async headers() {
